943b3cf9e6963e902d3e712c10b1f57a80b6b3cb
juliocesarscheidt <julio.scheidt96@gmail.com>
2024-01-26 19:50:17 -0300

    Add class

--- qrcode/generate.py
+++ qrcode/generate.py
@@ -2,3 +2,8 @@ import segno
 
 video = segno.make('https://www.youtube.com/@JulioScheidt?sub_confirmation=1')
 video.save('channel.png', dark="black", light="white", scale=10)
+
+# from segno import helpers
+
+# qrcode = helpers.make_wifi(ssid='<SSID>', password='<PASSWORD>', security='WPA')
+# qrcode.save("Wifi.png", dark="black", light="white", scale=10)
