f8f7243f5651e8ba17c627ead93a61e395db75b3
juliocesarscheidt <julio.scheidt96@gmail.com>
2024-04-26 21:16:34 -0300

    Add code

--- reference_vs_value/reference_vs_value_complex.py
+++ reference_vs_value/reference_vs_value_complex.py
@@ -33,3 +33,25 @@ print(id(list_a))
 # example: 1854078226568
 print(id(list_b))
 # example: 1854078226568
+
+
+# copying objects without the same reference
+import copy
+
+# https://docs.python.org/3/library/copy.html#module-copy
+# shallow copy
+list_c = copy.copy(list_a)
+# deep copy
+# list_c = copy.deepcopy(list_a)
+
+# or using the slice of the entire list
+# list_c = list_a[:]
+
+print(list_c)
+# [10000, 1, 2, 3, 4, 5, 6]
+list_c.append(7)
+
+print(list_c)
+# [10000, 1, 2, 3, 4, 5, 6, 7]
+print(list_a)
+# [10000, 1, 2, 3, 4, 5, 6]
