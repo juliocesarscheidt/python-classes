a76eca8c140ee4386a013fdb6c11936da813c981
juliocesarscheidt <julio.scheidt96@gmail.com>
2024-05-24 20:13:49 -0300

    Adjust generator code

--- generator/generator.py
+++ generator/generator.py
@@ -1,55 +1,44 @@
 """
-Um gerador (generator) é um objeto que pode ser chamado (callable), que atua como um iterável (iterable) (objeto no qual você pode iterar para ciclos)
+Um gerador (generator) Ã© um objeto que pode ser chamado (callable), que atua como um iterÃ¡vel (iterable) (objeto no qual vocÃª pode iterar para ciclos)
 
 Usado para controlar iteradores de loops, gera respostas sob demanda
 """
+import time
+
 
 # with yield generator
-def impar(elems):
+def par(elems):
   for x in elems:
-    if x % 2 != 0:
-      yield x
+    if x % 2 == 0: yield x
 
-# impar(range(20))
-# <generator object impar at 0x7a6d85943df0>
+generator1 = par([0, 1, 2, 3])
 
-list(impar(range(20)))
-# [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+try:
+  print(next(generator1))
+  # 0
+  print(next(generator1))
+  # 2
+  print(next(generator1))
+except StopIteration:
+  print("StopIteration")
 
 
+print(list(par([0, 1, 2, 3])))
+# [0, 2]
 
-def check_impar(x):
-  if x % 2 != 0:
-    yield x
 
-for x in range(20):
+generator2 = par([4, 5, 6, 7])
+
+while True:
   try:
-    next(check_impar(x))
+    print(next(generator2))
+    time.sleep(2)
   except StopIteration:
-    pass
+    print("StopIteration")
+    break
 
-
-
-# with list comprehension
-def impar(elems):
-  return [x for x in elems if x %2 != 0]
-
-# impar(range(20))
-# [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
-
-
-
-# with filter and lambda
-def impar(elems):
-  return filter(lambda x: x %2 != 0, elems)
-# [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
-
-# impar(range(20))
-# <filter object at 0x7a6d8588da20>
-
-list(impar(range(20)))
-# [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
-
-
-
-
+"""
+4
+6
+StopIteration
+"""
