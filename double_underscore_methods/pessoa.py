aa49740aacbd0a670c2921ffa65f7c6d171d79c2
juliocesarscheidt <julio.scheidt96@gmail.com>
2024-05-25 19:41:37 -0300

    Improve script

--- double_underscore_methods/pessoa.py
+++ double_underscore_methods/pessoa.py
@@ -1,3 +1,12 @@
+"""
+  double underscore methods ou magic methods
+
+  Sao metodos especiais que voce pode definir para adicionar alguma “magica” as suas classes,
+  algum comportamento que sera invocado em alguma operacao do python
+
+  Eles sao sempre cercados por __ (double underscore)
+"""
+
 # double underscore methods da classe object
 # dir(object)
 # ['__class__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__']
