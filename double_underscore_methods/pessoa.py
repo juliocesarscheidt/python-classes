5483539a4f26628515feb9877be14e9def2ceacb
juliocesarscheidt <julio.scheidt96@gmail.com>
2024-05-25 19:18:05 -0300

    Improve script

--- double_underscore_methods/pessoa.py
+++ double_underscore_methods/pessoa.py
@@ -1,31 +1,44 @@
+# double underscore methods da classe object
+# dir(object)
+# ['__class__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__']
+
 class Pessoa():
   # propriedades
   nome: str
 
-  # construtor
+  # construtor - metodo chamado quando esta classe e instanciada
   def __init__(self, nome: str) -> None:
     print('__init__ called')
     self.nome = nome
-
+  
+  # metodo chamado quando esta classe e herdada
+  def __init_subclass__(self) -> None:
+    print('__init_subclass__ called')
+  
+  # metodo de igualdade usado no ==
   def __eq__(self, other: object) -> bool:
     print('__eq__ called')
     if isinstance(other, Pessoa):
       if other.nome == self.nome:
         return True
     return False
-
+  
+  # negacao de igualdade usado no !=
   def __ne__(self, other: object) -> bool:
     print('__ne__ called')
     return not self.__eq__(other)
-
+  
+  # metodo que retorna algo quando solicitado a mostrar aos humanos (ex. print)
   def __str__(self) -> str:
     print('__str__ called')
     return f"Pessoa str {self.nome}"
 
+  # metodo que retorna o que você precisa ver sobre sua classe em mensagens de depuracao
   def __repr__(self) -> str:
     print('__repr__ called')
     return f"Pessoa repr {self.nome}"
-
+  
+  # metodo para iterar no objeto
   def __iter__(self):
     print('__iter__ called')
     for letter in self.nome:
@@ -42,24 +55,32 @@ class Pessoa():
     self._nome = value  # getter
 
 
-pessoa1 = Pessoa("Julio")
+class PessoaFilho(Pessoa):
+  pass
+  
+# __init_subclass__ called
+
+
+pessoa1 = Pessoa("John")
+# __init__ called
 
 pessoa1
-# Pessoa repr Julio
+# Pessoa repr John
 
 print(pessoa1)
 # __str__ called
-# Pessoa str Julio
+# Pessoa str John
 
-pessoa2 = Pessoa("Julio")
+pessoa2 = Pessoa("John")
+# __init__ called
 
 # compares value
-print(pessoa1 == pessoa1)
+print(pessoa1 == pessoa2)
 # __eq__ called
 # True
 
 # compares reference
-print(pessoa1 is pessoa1)
+print(pessoa1 is pessoa2)
 # False
 
 
@@ -68,8 +89,7 @@ for letter in pessoa1:
 """
 __iter__ called
 -------- J --------
--------- u --------
--------- l --------
--------- i --------
 -------- o --------
+-------- h --------
+-------- n --------
 """
