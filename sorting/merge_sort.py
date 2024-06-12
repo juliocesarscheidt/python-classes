7faf1e60264ce6f8b02aa309f94a16d2008db12b
juliocesarscheidt <julio.scheidt96@gmail.com>
2024-06-12 16:23:19 -0300

    Sorting algorithms

---
+++ sorting/merge_sort.py
@@ -0,0 +1,56 @@
+from typing import List
+
+def merge(arr_a: List[int], arr_b: List[int]):
+  # print('---------- merge ----------')
+  # print('arr_a', arr_a, 'arr_b', arr_b)
+  arr_result = []
+
+  while (
+    arr_a is not None and len(arr_a) > 0 and \
+    arr_b is not None and len(arr_b) > 0 \
+  ):
+    if arr_a[0] > arr_b[0]:
+      # remove from the beginning of arr_b and append to the final of arr_result
+      arr_result.append(arr_b.pop(0))
+    else:
+      # remove from the beginning of arr_a and append to the final of arr_result
+      arr_result.append(arr_a.pop(0))
+
+  # at this point either arr_a or arr_b is empty
+
+  while arr_a is not None and len(arr_a) > 0:
+    # remove from the beginning of arr_a and append to the final of arr_result
+    arr_result.append(arr_a.pop(0))
+
+  while arr_b is not None and len(arr_b) > 0:
+    # remove from the beginning of arr_b and append to the final of arr_result
+    arr_result.append(arr_b.pop(0))
+
+  # print('arr_result', arr_result)
+  return arr_result
+
+def mergesort(arr: List[int]):
+  # print('-------------------- mergesort --------------------')
+  if len(arr) <= 1:
+    # print('arr', arr)
+    return arr
+  
+  half_index = len(arr)//2
+  # print('half_index', half_index)
+  
+  arr_a = arr[0:half_index]
+  arr_b = arr[half_index:]
+  # print('arr_a', arr_a, 'arr_b', arr_b)
+  
+  return merge(
+    mergesort(arr_a),
+    mergesort(arr_b),
+  )
+
+arr = [4, 2, 5, 1, 6, 3, 7]
+print(mergesort(arr))
+# [1, 2, 3, 4, 5, 6, 7]
+
+arr = [12, 45, 13, 5, 35, 18, 26]
+print(mergesort(arr))
+# [5, 12, 13, 18, 26, 35, 45]
--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -1,38 +1,50 @@
 from typing import List
 
-def partition(values: List[int], esquerda: int, direita: int):
-  pivo = values[esquerda] # setando o pivo para ficar na esquerda
+def partition(arr: List[int], esquerda: int, direita: int):
+  # print('---------- partition ----------')
+  # print('partition BEFORE arr', arr)
+  pivo = arr[esquerda] # setando o pivo para ficar na esquerda
   i = esquerda
   j = direita
+  # print('partition pivo', pivo, '| i', i, '| j', j)
 
   while i < j:
-    while values[i] <= pivo and i < direita:
+    # print('partition while BEFORE i', i, '| j', j)
+    while arr[i] <= pivo and i < direita:
       i+=1
-    while values[j] > pivo:
+    while arr[j] > pivo:
       j-=1
+    # print('partition while AFTER i', i, '| j', j)
     if i < j:
       # inverte os valores
-      values[i], values[j] = values[j], values[i]
-
-  values[esquerda] = values[j]
-  values[j] = pivo
+      arr[i], arr[j] = arr[j], arr[i]
+      # print('partition swapping arr', arr)
+  
+  # print('partition AFTER arr', arr, '| esquerda', esquerda, '| direita', direita)
+  
+  arr[esquerda] = arr[j]
+  arr[j] = pivo
+  
+  # print('partition AFTER swapping arr', arr, '| esquerda', esquerda, '| direita', direita)
   return j
 
-def quicksort_recursive(values: List[int], esquerda: int, direita: int):
+def quicksort(arr: List[int], esquerda: int, direita: int):
+  # print('-------------------- quicksort --------------------')
   i = 0
   if direita > esquerda:
-    i = partition(values, esquerda, direita)
-    quicksort_recursive(values, esquerda, i-1)
-    quicksort_recursive(values, i+1, direita)
-  return values
+    # print('quicksort esquerda', esquerda, '| direita', direita)
+    i = partition(arr, esquerda, direita)
+    # print('quicksort i', i)
+    quicksort(arr, esquerda, i-1)
+    quicksort(arr, i+1, direita)
+  return arr
 
-def quicksort(values: List[int]):
-  esquerda = 0
-  direita = len(values) -1
-  return quicksort_recursive(values, esquerda, direita)
-
-print(quicksort([4, 2, 5, 1, 6, 3, 7]))
+arr = [4, 2, 5, 1, 6, 3, 7]
+esquerda, direita = 0, len(arr) -1
+print(quicksort(arr, esquerda, direita))
 # [1, 2, 3, 4, 5, 6, 7]
 
-print(quicksort([12, 45, 13, 5, 35, 18, 26]))
+arr = [12, 45, 13, 5, 35, 18, 26]
+esquerda, direita = 0, len(arr) -1
+print(quicksort(arr, esquerda, direita))
 # [5, 12, 13, 18, 26, 35, 45]
