a2a8c8e17df89d3baafb1e8de8680937fe4bc103
juliocesarscheidt <julio.scheidt96@gmail.com>
2024-08-25 00:14:20 -0300

    Improve class

--- walrus_operator/operator.py
+++ walrus_operator/operator.py
@@ -4,30 +4,34 @@
 
 import unicodedata
 
-allowed_operations = ['read', 'write']
-requestes_operations = ['reád', 'réad', 'READ', 'WRITÉ', 'DROP']
-
 def normalize(text):
+  # Normalization Form Compatibility Decomposition
   return unicodedata.normalize("NFKD", text).encode("ASCII", "ignore").decode("ASCII").lower()
-  # "NFKD" converts the characters removing the accentuation
-  # "NFC" removes the characters
 
 print(normalize('WRITÉ'))
 # write
 
-# list comprehensions using walrus operator :=
-# it will create the variable inside of the scope to be used further
-allowed_list = [clean_op for op in requestes_operations
-  if (clean_op := normalize(op)) in allowed_operations]
+
+allowed_operations = ['read', 'write']
+requested_operations = ['reád', 'réad', 'READ', 'WRITÉ', 'DROP']
+
+
+# list comprehension - sem o walrus operator
+allowed_list = [normalize(op) for op in requested_operations if normalize(op) in allowed_operations]
+
+# list comprehension - usando walrus operator :=
+# ele criará a variável dentro do escopo para ser usada posteriormente
+allowed_list = [clean_op for op in requested_operations if (clean_op := normalize(op)) in allowed_operations]
 
 print(allowed_list)
 # ['read', 'read', 'read', 'write']
 
-# variable still exists outside its creation scope
+
+# variável ainda existe fora do seu escopo de criação
 print(clean_op)
 # drop
 
-# if using walrus operator :=
+# if - usando walrus operator :=
 if (n := len(allowed_list)) > 1:
   print(f"List has at least 1 element => ({n} elements)")
 # List has at least 1 element => (4 elements)
